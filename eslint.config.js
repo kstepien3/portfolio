ee5d0f1e6a165f01e4f19204332ec851ce9a4803
Konrad Stępień <kord.stp@gmail.com>
2025-10-25 16:18:49 +0200

    ci(eslint): improve configuration (#18)

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,12 @@
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
 const angular = require('angular-eslint');
+const eslintPluginImport = require('eslint-plugin-import');
+const unicorn = require('eslint-plugin-unicorn');
+const unusedImports = require('eslint-plugin-unused-imports');
+const simpleImportSort = require('eslint-plugin-simple-import-sort');
 const eslintConfigPrettier = require('eslint-config-prettier');
+const prettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = tseslint.config(
   {
@@ -16,6 +21,12 @@ module.exports = tseslint.config(
       eslintConfigPrettier,
     ],
     processor: angular.processInlineTemplates,
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      import: eslintPluginImport,
+      unicorn: unicorn,
+      'unused-imports': unusedImports,
+    },
     rules: {
       '@angular-eslint/directive-selector': [
         'error',
@@ -35,7 +46,9 @@ module.exports = tseslint.config(
       ],
 
       // Angular best practices
-      '@angular-eslint/no-empty-lifecycle-method': 'warn',
+      '@angular-eslint/no-empty-lifecycle-method': 'error',
+      '@angular-eslint/no-input-rename': 'off',
+      '@angular-eslint/no-output-native': 'warn', // remove?
       '@angular-eslint/prefer-on-push-component-change-detection': 'warn',
       '@angular-eslint/prefer-output-readonly': 'warn',
       '@angular-eslint/prefer-signals': 'warn',
@@ -62,10 +75,24 @@ module.exports = tseslint.config(
       ],
       '@typescript-eslint/no-empty-function': 'warn',
       '@typescript-eslint/no-empty-interface': 'error',
+      '@typescript-eslint/no-empty-object-type': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-inferrable-types': 'warn',
       '@typescript-eslint/no-shadow': 'warn',
+      '@typescript-eslint/no-this-alias': 'error',
+      '@typescript-eslint/no-unsafe-function-type': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
+
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      'no-case-declarations': 'warn',
+      'prefer-rest-params': 'warn',
+
+      // imports / exports
+      'import/no-duplicates': ['warn', { considerQueryString: true }],
+      'unused-imports/no-unused-imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+      'simple-import-sort/imports': 'warn',
 
       // JavaScript best practices
       eqeqeq: 'error',
@@ -109,28 +136,70 @@ module.exports = tseslint.config(
     files: ['**/*.html'],
     extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
     rules: {
+      '@angular-eslint/template/use-track-by-function': 'warn',
+      '@angular-eslint/template/alt-text': 'warn',
+      '@angular-eslint/template/click-events-have-key-events': 'warn',
+      '@angular-eslint/template/eqeqeq': 'error',
+      '@angular-eslint/template/interactive-supports-focus': 'warn',
+      '@angular-eslint/template/label-has-associated-control': 'warn',
+      '@angular-eslint/template/prefer-self-closing-tags': 'warn',
+      '@angular-eslint/template/prefer-ngsrc': 'warn',
+      '@angular-eslint/template/prefer-control-flow': 'error',
+      '@angular-eslint/template/cyclomatic-complexity': ['warn', { maxComplexity: 10 }],
+      '@angular-eslint/template/elements-content': [
+        'error',
+        {
+          allowList: ['label'],
+        },
+      ],
       // Angular template best practices
       '@angular-eslint/template/attributes-order': [
-        'error',
+        'warn',
         {
           alphabetical: true,
           order: [
-            'STRUCTURAL_DIRECTIVE', // deprecated, use @if and @for instead
-            'TEMPLATE_REFERENCE', // e.g. `<input #inputRef>`
-            'ATTRIBUTE_BINDING', // e.g. `<input required>`, `id="3"`
-            'INPUT_BINDING', // e.g. `[id]="3"`, `[attr.colspan]="colspan"`,
-            'TWO_WAY_BINDING', // e.g. `[(id)]="id"`,
-            'OUTPUT_BINDING', // e.g. `(idChange)="handleChange()"`,
+            'STRUCTURAL_DIRECTIVE',
+            'TEMPLATE_REFERENCE',
+            'ATTRIBUTE_BINDING',
+            'INPUT_BINDING',
+            'TWO_WAY_BINDING',
+            'OUTPUT_BINDING',
           ],
         },
       ],
-      '@angular-eslint/template/button-has-type': 'warn',
-      '@angular-eslint/template/cyclomatic-complexity': ['warn', { maxComplexity: 10 }],
-      '@angular-eslint/template/eqeqeq': 'error',
-      '@angular-eslint/template/prefer-control-flow': 'error',
-      '@angular-eslint/template/prefer-ngsrc': 'warn',
-      '@angular-eslint/template/prefer-self-closing-tags': 'warn',
-      '@angular-eslint/template/use-track-by-function': 'warn',
+    },
+  },
+  {
+    files: ['**/ui/**/*.ts'],
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'zen',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: ['attribute', 'element'],
+          prefix: 'zen',
+          style: 'kebab-case',
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.html', '**/*.ts'],
+    extends: [prettierRecommended],
+    rules: {
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto',
+        },
+      ],
     },
   }
 );
