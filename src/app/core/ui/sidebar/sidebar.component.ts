b9005d686345eb8a831f7a49715d884717b4d818
Konrad Stępień <kord.stp@gmail.com>
2025-10-27 07:08:33 +0100

    feat(sidebar): add toggle functionality

--- src/app/core/ui/sidebar/sidebar.component.ts
+++ src/app/core/ui/sidebar/sidebar.component.ts
@@ -1,9 +1,14 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'zen-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[attr.data-opened]': 'opened()',
+  },
 })
-export class Sidebar {}
+export class Sidebar {
+  readonly opened = input<boolean>(false);
+}
